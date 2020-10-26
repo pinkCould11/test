95c2d2c6fe3ee6c878a50aee7b976bdf90eca643
pinkCould11 <72103126+pinkCould11@users.noreply.github.com>
2020-10-26 14:10:26 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  List
+//
+//  Created by Hana Keinan on 10/10/20.
+//
+import CoreData
+import SwiftUI
+
+struct Test{
+    let task :Task
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
